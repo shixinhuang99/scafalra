7bf902d4995c95469f4b2e4fa1ef00c29b8dbbea
zerowong <wongzero@foxmail.com>
2022-07-17 03:04:38 +0800

    chore: update publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,6 +3,8 @@ set -e
 readonly build="npm run build"
 readonly test="npm run test"
 readonly publish="npm publish"
+readonly push="git push"
+readonly pushTags="git push --tags"
 
 function check {
   if [ $? -ne 0 ]; then
@@ -15,3 +17,5 @@ check
 ${test}
 check
 ${publish}
+${push}
+${pushTags}
