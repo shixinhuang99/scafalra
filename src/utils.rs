460979201bad718318d6adcd5f4fdfd0abaeed55
shixinhuang99 <shixinhuang99@gmail.com>
2023-03-30 02:34:25 +0800

    feat: proxy agent

---
+++ src/utils.rs
@@ -0,0 +1,17 @@
+use std::env;
+
+use ureq::{Agent, AgentBuilder, Proxy};
+
+pub fn build_proxy_agent() -> Agent {
+    let env_proxy = env::var("https_proxy").or(env::var("http_proxy"));
+    let agent = AgentBuilder::new();
+
+    if let Ok(env_proxy) = env_proxy {
+        let proxy = Proxy::new(env_proxy);
+        if let Ok(proxy) = proxy {
+            return agent.proxy(proxy).build();
+        }
+    }
+
+    agent.build()
+}
