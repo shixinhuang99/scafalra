2feec313090fa02bfc1d387a43c0a8f8e507f218
shixinhuang99 <shixinhuang99@gmail.com>
2023-03-30 02:34:30 +0800

    feat: impl repository unzip and optmize unit tests

--- src/repotitory.rs
+++ src/repotitory.rs
@@ -2,13 +2,15 @@
 
 use std::fs::{self, File};
 use std::io;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
+use anyhow::{anyhow, Result};
 use once_cell::sync::Lazy;
 use regex::Regex;
+use remove_dir_all::remove_dir_all;
 use zip::ZipArchive;
 
-type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+use crate::utils::build_proxy_agent;
 
 static REPO_RE: Lazy<Regex> = Lazy::new(|| {
     let re = r"^([^/\s]+)/([^/\s?]+)(?:((?:/[^/\s?]+)+))?(?:\?(branch|tag|commit)=([^\s]+))?$";
@@ -19,10 +21,11 @@ static REPO_RE: Lazy<Regex> = Lazy::new(|| {
 pub struct Repository {
     pub owner: String,
     pub name: String,
-    pub subdir: Option<String>,
+    pub subdir: Option<PathBuf>,
     pub query: Option<Query>,
 }
 
+#[derive(PartialEq, Debug)]
 pub enum Query {
     BRANCH(String),
     TAG(String),
@@ -30,20 +33,26 @@ pub enum Query {
 }
 
 impl Repository {
-    fn new(input: &String) -> Result<Self> {
+    fn new(input: &str) -> Result<Self> {
         let caps = REPO_RE
             .captures(input)
-            .ok_or(format!("Could not parse the input: '{}'.", input))?;
+            .ok_or(anyhow!("Could not parse the input: '{}'", input))?;
 
         let owner = (&caps[1]).to_string();
         let name = (&caps[2]).to_string();
-        let subdir = caps.get(3).map_or(None, |v| Some(v.as_str().to_string()));
+
+        let subdir = caps
+            .get(3)
+            .map_or(None, |v| Some(PathBuf::from(v.as_str())));
+
         let query_type = caps.get(4).map_or(None, |v| Some(v.as_str()));
-        let query_val = (&caps[5]).to_string();
-        let query = match query_type {
-            Some("branch") => Some(Query::BRANCH(query_val)),
-            Some("tag") => Some(Query::TAG(query_val)),
-            Some("commit") => Some(Query::COMMIT(query_val)),
+        let query_val =
+            caps.get(5).map_or(None, |v| Some(v.as_str().to_string()));
+
+        let query = match (query_type, query_val) {
+            (Some("branch"), Some(val)) => Some(Query::BRANCH(val)),
+            (Some("tag"), Some(val)) => Some(Query::TAG(val)),
+            (Some("commit"), Some(val)) => Some(Query::COMMIT(val)),
             _ => None,
         };
 
@@ -54,22 +63,58 @@ impl Repository {
             query,
         })
     }
+
+    pub fn cache(
+        &self,
+        zipball_url: &str,
+        parent_dir: &Path,
+        oid: &str,
+    ) -> Result<()> {
+        let repo_dir_path = PathBuf::from(format!(
+            "{}-{}-{}",
+            self.owner,
+            self.name,
+            &oid[0..7]
+        ));
+
+        let zip_file_path = repo_dir_path.with_extension("zip");
+
+        download(zipball_url, &zip_file_path)?;
+
+        if repo_dir_path.exists() {
+            remove_dir_all(repo_dir_path)?;
+        }
+
+        unzip(&zip_file_path, parent_dir)?;
+
+        fs::remove_file(zip_file_path)?;
+
+        Ok(())
+    }
+
+    pub fn is_repo(input: &str) -> bool {
+        REPO_RE.is_match(input)
+    }
 }
 
 fn download(url: &str, zip_file_path: &Path) -> Result<()> {
-    let response = ureq::get(&url).call()?;
+    let agent = build_proxy_agent();
+    let response = agent.get(url).call()?;
     let mut file = File::create(zip_file_path)?;
+
     io::copy(&mut response.into_reader(), &mut file)?;
 
     Ok(())
 }
 
-fn unzip(zip_file_path: &Path, target_dir_path: &Path) -> Result<()> {
+fn unzip(zip_file_path: &Path, parent_dir: &Path) -> Result<()> {
     let file = File::open(zip_file_path)?;
     let mut archive = ZipArchive::new(file)?;
+
     for i in 0..archive.len() {
         let mut file = archive.by_index(i)?;
-        let outpath = Path::new(target_dir_path).join(file.name());
+        let outpath = Path::new(&parent_dir).join(file.name());
+
         if file.is_dir() {
             fs::create_dir_all(&outpath)?;
         } else {
@@ -82,6 +127,7 @@ fn unzip(zip_file_path: &Path, target_dir_path: &Path) -> Result<()> {
             io::copy(&mut file, &mut outfile)?;
         }
     }
+
     Ok(())
 }
 
@@ -169,45 +215,51 @@ mod tests {
     }
 
     #[test]
-    fn repo_new_ok() {
-        let repo = Repository::new(
-            &"test/repository/path/to/file?branch=main".to_string(),
-        );
+    fn repo_new_basic() {
+        let repo = Repository::new("test/repository");
         assert!(repo.is_ok());
         let repo = repo.unwrap();
         assert_eq!("test", &repo.owner);
         assert_eq!("repository", &repo.name);
-        assert_eq!("/path/to/file", &repo.subdir.unwrap());
-        if let Query::BRANCH(val) = &repo.query.unwrap() {
-            assert_eq!("main", val.as_str());
-        } else {
-            panic!("");
-        };
+    }
+
+    #[test]
+    fn repo_new_with_all() {
+        let repo = Repository::new("test/repository/path/to/file?branch=main");
+        assert!(repo.is_ok());
+        let repo = repo.unwrap();
+        assert_eq!("test", &repo.owner);
+        assert_eq!("repository", &repo.name);
+        assert_eq!("/path/to/file", repo.subdir.unwrap().to_str().unwrap());
+        assert_eq!(Query::BRANCH("main".to_string()), repo.query.unwrap());
     }
 
     #[test]
     fn repo_new_err() {
-        let repo = Repository::new(&"test".to_string());
+        let repo = Repository::new("test");
         assert!(repo.is_err());
+    }
+
+    #[test]
+    fn is_repo_ok() {
+        assert_eq!(true, Repository::is_repo("foo/bar"));
+        assert_eq!(false, Repository::is_repo("foo"));
     }
 
     #[ignore]
     #[test]
-    fn download() {
-        use std::fs;
+    fn cache_repo() {
         use std::path::Path;
 
-        let url = "https://codeload.github.com/shixinhuang99/scafalra/legacy.zip/ ea7c165bac336140bcf08f84758ab752769799be";
+        use super::Repository;
 
-        let zip_file_name = "shixinhuang99+scafalra+ea7c165bac336140bcf08f84758ab752769799be.zip";
+        let url = "https://codeload.github.com/shixinhuang99/scafalra/legacy.zip/ea7c165bac336140bcf08f84758ab752769799be";
+        let dir = Path::new("something");
+        let oid = "ea7c165bac336140bcf08f84758ab752769799be";
 
-        let path = Path::new(zip_file_name);
-
-        super::download(url, &path).unwrap();
-
-        assert!(path.exists());
-        assert_eq!(zip_file_name, path.file_name().unwrap());
-
-        fs::remove_file(&path).unwrap();
+        let repo = Repository::new("shixinhuang99/scafalra");
+        assert!(repo.is_ok());
+        let repo = repo.unwrap();
+        assert!(repo.cache(url, dir, oid).is_ok());
     }
 }
