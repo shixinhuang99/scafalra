776610166f2d05333dcd28428e2f6a1e037f2b3e
ShiXin Huang <shixinhuang99@gmail.com>
2023-12-09 15:18:25 +0800

    fix: json parse error when content is empty (#33)

--- src/json.rs
+++ src/json.rs
@@ -8,14 +8,20 @@ where
 	Self: DeserializeOwned + Serialize + Default,
 {
 	fn load(file_path: &Utf8Path) -> Result<Self> {
-		let content: Self = if file_path.exists() {
-			serde_json::from_str(&fs::read_to_string(file_path)?)?
-		} else {
-			fs::File::create(file_path)?;
-			Self::default()
+		let write_and_return_default = || -> Result<Self> {
+			let default = Self::default();
+			fs::write(file_path, serde_json::to_string_pretty(&default)?)?;
+			Ok(default)
 		};
-
-		Ok(content)
+		if file_path.exists() {
+			let content = fs::read_to_string(file_path)?;
+			if content.is_empty() {
+				return write_and_return_default();
+			}
+			let value: Self = serde_json::from_str(&content)?;
+			return Ok(value);
+		}
+		write_and_return_default()
 	}
 
 	fn save(&self, file_path: &Utf8Path) -> Result<()> {
@@ -54,13 +60,20 @@ mod tests {
 
 	impl JsonContent for Foo {}
 
-	fn mock_foo(create_file: bool) -> Result<(Foo, TempDir, Utf8PathBuf)> {
+	fn mock_foo(
+		create_file: bool,
+		init_content: bool,
+	) -> Result<(Foo, TempDir, Utf8PathBuf)> {
 		let temp_dir = tempdir()?;
 		let file_path =
 			temp_dir.path().join("foo.json").into_utf8_path_buf()?;
 
 		if create_file {
-			fs::write(&file_path, "{\n  \"bar\": \"bar\"\n}")?;
+			if init_content {
+				fs::write(&file_path, "{\n  \"bar\": \"bar\"\n}")?;
+			} else {
+				fs::write(&file_path, "")?;
+			}
 		}
 
 		let foo = Foo::load(&file_path)?;
@@ -70,7 +83,7 @@ mod tests {
 
 	#[test]
 	fn test_load_file_exists() -> Result<()> {
-		let (foo, _dir, _file_path) = mock_foo(true)?;
+		let (foo, _dir, _file_path) = mock_foo(true, true)?;
 
 		assert_eq!(foo.bar, "bar");
 
@@ -78,8 +91,17 @@ mod tests {
 	}
 
 	#[test]
+	fn test_load_file_exists_empty_content() -> Result<()> {
+		let (foo, _tmpdir, _file_path) = mock_foo(true, false)?;
+
+		assert_eq!(foo.bar, "");
+
+		Ok(())
+	}
+
+	#[test]
 	fn test_load_file_not_exists() -> Result<()> {
-		let (foo, _dir, file_path) = mock_foo(false)?;
+		let (foo, _dir, file_path) = mock_foo(false, false)?;
 
 		assert_eq!(foo.bar, "");
 		assert!(file_path.exists());
@@ -89,7 +111,7 @@ mod tests {
 
 	#[test]
 	fn test_save() -> Result<()> {
-		let (mut foo, _dir, file_path) = mock_foo(true)?;
+		let (mut foo, _dir, file_path) = mock_foo(true, true)?;
 
 		foo.bar = "bar2".to_string();
 		foo.save(&file_path)?;
