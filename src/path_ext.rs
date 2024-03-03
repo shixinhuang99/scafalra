dc0dd6981f33d36b2931b805bf560929904828a1
ShiXin Huang <shixinhuang99@gmail.com>
2024-03-03 20:15:52 +0800

    chore(deps): bump rust toolchain to nightly-2024-02-08

--- src/path_ext.rs
+++ src/path_ext.rs
@@ -1,19 +1,16 @@
 use std::path::{Path, PathBuf};
 
+#[cfg(test)]
 use path_slash::PathBufExt;
 
+#[cfg(test)]
 pub trait JoinSlash {
 	fn join_slash<T>(&self, s: T) -> PathBuf
 	where
 		T: AsRef<str>;
 }
 
-pub trait JoinIter<I> {
-	fn join_iter<T>(&self, iter: T) -> PathBuf
-	where
-		T: IntoIterator<Item = I>;
-}
-
+#[cfg(test)]
 impl JoinSlash for Path {
 	fn join_slash<T>(&self, s: T) -> PathBuf
 	where
@@ -21,6 +18,12 @@ impl JoinSlash for Path {
 	{
 		self.join(PathBuf::from_slash(s))
 	}
+}
+
+pub trait JoinIter<I> {
+	fn join_iter<T>(&self, iter: T) -> PathBuf
+	where
+		T: IntoIterator<Item = I>;
 }
 
 impl<I: AsRef<Path>> JoinIter<I> for Path {
